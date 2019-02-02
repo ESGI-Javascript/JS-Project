a7ce5da2d1a6adec0b150dcd7e994278d5050ac6
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2019-02-02 12:34:40 +0100

    créer et dessiner la pièce avec décalage

--- public/js/tetris.js
+++ public/js/tetris.js
@@ -6,3 +6,30 @@ const screen = new createCanvas(document.getElementById('tetris'));
 let canvas = prop_access(screen, 'area');
 let context = screen.context;
 
+//création de la pièce
+const tetrisMatrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+];
+
+//param : tetrisMatrix et décalage
+//fonction : dessiner la pièce
+function drawTetrisMatrix(matrix, offset){
+    tetrisMatrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if(value !== 0){
+                context.fillStyle = 'red';
+                //on utilise le décalage pour l'ordonnée y et l'abscisse x
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        });
+    });
+}
+
+const values = {
+    position : {x: 5, y:5},
+    matrix: tetrisMatrix,
+}
+
+drawTetrisMatrix(values.matrix, values.position);
