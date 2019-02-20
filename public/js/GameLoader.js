24c07df8baef20904707933897abb45191f51819
unknown <aymanezaircha@gmail.com>
2019-02-20 22:26:54 +0100

    ajout de quelque fonction

--- public/js/GameLoader.js
+++ public/js/GameLoader.js
@@ -32,6 +32,74 @@ export default class GameLoader
 	    user.updateScore();
 	    scroll.start();
 	}
+
+	/*
+
+		//création de la pièce
+		const tetrisMatrix = [
+		    [0, 0, 0],
+		    [1, 1, 1],
+		    [0, 1, 0],
+		];
+
+		function draw(){
+		    context.fillStyle = '#000';
+		    context.fillRect(0, 0, canvas.width, canvas.height);
+
+		    drawTetrisMatrix(values.matrix, values.position);
+		    updateScore();
+		}
+
+		//param : tetrisMatrix et décalage
+		//fonction : dessiner la pièce
+		function drawTetrisMatrix(matrix, offset){
+		    tetrisMatrix.forEach((row, y) => {
+		        row.forEach((value, x) => {
+		            if(value !== 0){
+		                context.fillStyle = 'red';
+		                //on utilise le décalage pour l'ordonnée y et l'abscisse x
+		                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+		            }
+		        });
+		    });
+		}
+
+
+		//création de la pièce aléatoirement
+		function piecesRandom() {*/
+		    /*const pieces = 'TJLOSZI';
+		    values.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+		    values.position.y = 0;*/
+	/*	}
+
+
+		//afficher le score de la partie
+		function updateScore() {
+		    document.getElementById('score').innerText = values.score;
+		}
+
+		//event pour deplacer la piece
+		document.addEventListener('keydown', event => {
+		    if (event.key === "ArrowLeft") {
+		        console.log("ArrowLeft");
+		    } else if (event.key === "ArrowRight") {
+		        console.log("ArrowRight");
+		    } else if (event.key === "ArrowDown") {
+		        console.log("ArrowDown");
+		    } else if (event.key === "q") {
+		        console.log("key q");
+		    } else if (event.key === "d") {
+		        console.log("key d");
+		    }
+		});
+
+		const values = {
+		    position : {x: 5, y:5},
+		    matrix: tetrisMatrix,
+		    score: 0
+		};*/
+
+
 }
 
 
