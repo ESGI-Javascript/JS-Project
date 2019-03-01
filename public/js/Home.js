cc8b7b282c5d99eb5af58306b536a48a86e99d15
HugoChea <42536588+HugoChea@users.noreply.github.com>
2019-03-01 01:13:21 +0100

    Update Home.js

--- public/js/Home.js
+++ public/js/Home.js
@@ -3,5 +3,8 @@ export default class Home
 	load()
 	{
 		document.getElementById("homeindex").style.display = "none";
+		document.getElementById("newGame").onclick = function() {
+            	localStorage.setItem('name', document.getElementById("nameInput").value);
+       		};
 	}
 }
